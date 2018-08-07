c4451d4dd11a4f714e666f871ea5eef50492458b
ektoras <hector.drandakis@gmail.com>
2018-08-07 21:14:23 +0300

    final implementation

--- src/PID.cpp
+++ src/PID.cpp
@@ -16,20 +16,20 @@ void PID::Init(double Kp, double Ki, double Kd) {
     this->Kp = Kp;
     this->Ki = Ki;
     this->Kd = Kd;
+
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
 
-//    if (p_error == numeric_limits<double>::max()) {
-//        p_error = cte;
-//    }
     d_error = cte - p_error;
     p_error = cte;
     i_error += cte;
 }
 
 double PID::TotalError() {
-//    return Kp * p_error + Kd * d_error + Ki * i_error;
-    return -Kp * p_error - Kd * d_error - Ki * i_error;
+    return (-Kp * p_error) - (Kd * d_error) - (Ki * i_error);
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -32,14 +32,14 @@ int main()
 {
   uWS::Hub h;
 
-  PID pid;
-  // TODO: Initialize the pid variable.
-  double kp = 0.2;
-  double ki = 0.004;
-  double kd = 3.0;
-  pid.Init(kp, ki, kd);
+  PID pid, speed_pid;
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+  // Initialize the pid variable.
+  pid.Init(0.09, 0.001, 14);
+  // Initialize the pid variable.
+  speed_pid.Init(0.24, 0.005, 14);
+
+  h.onMessage([&pid, &speed_pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -55,14 +55,27 @@ int main()
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
+          double speed_error = 0;
+          double default_speed = 25;
           /*
           * TODO: Calcuate steering value here, remember the steering value is
           * [-1, 1].
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+
+          // Set steering
+          pid.d_error = cte;
+
           pid.UpdateError(cte);
-          steer_value = -pid.TotalError();
+          steer_value = pid.TotalError();
+
+          // Set speed
+          speed_error = speed - default_speed; // Set speed error
+          speed_pid.d_error = speed_error;
+
+          speed_pid.UpdateError(speed_error);
+          speed_pid.TotalError();
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
